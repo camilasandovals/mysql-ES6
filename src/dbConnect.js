c54e78c0f598d680572c65cbb481116b439931d2
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-08 19:46:39 -0400

    added more functions

--- src/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import  mysql  from 'mysql2'
-
-
-import { credentials } from '../credentials.js'
-
-
-export const db = mysql.createConnection(credentials);
--- src/readAll.js
+++
@@ -1,9 +0,0 @@
-import { db } from "./dbConnect.js"
-
-db.query("SELECT * FROM customers", (err, data) =>{
-    if(err){
-        console.error(err);
-    }
-    console.table(data);
-    db.end();
-});
