5a04bb9b49252b759df3f863604f724f878d8c92
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-09 18:10:07 -0400

    created readOne function

---
+++ src/readOne.js
@@ -0,0 +1,10 @@
+import { db } from "./dbConnect.js"
+
+db.query("SELECT * FROM customers WHERE firstName = 'Sergio'", (err, data) =>{
+    if(err){
+        console.error(err);
+    }
+    console.table(data);
+    db.end();
+});
+ 
